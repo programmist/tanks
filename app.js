cd0886f03f337314358f0430dbfcc8aeabc8ad81
Tony Childs <tchilds@zixcorp.com>
2013-09-10 13:11:18 -0500

    integrated socket.io into express.

--- app.js
+++ app.js
@@ -1,38 +1,31 @@
-/**
- * Module dependencies.
- */
 
 var express = require('express');
 var routes = require('./routes');
 var user = require('./routes/user');
-var http = require('http');
-var path = require('path');
 
 var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+var path = require('path');
 
-// all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(express.cookieParser('your secret here'));
-app.use(express.session());
 app.use(app.router);
 app.use(require('less-middleware')({ src: __dirname + '/public' }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.directory(__dirname + '/public'));
-// development only
-if ('development' == app.get('env')) {
-    app.use(express.errorHandler());
-}
 
 app.get('/', routes.index);
 app.get('/users', user.list);
 //app.get('/images', images.list);
 
-http.createServer(app).listen(app.get('port'), function () {
+server.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
+});
+
+io.sockets.on('connection', function (socket) {
+    socket.emit('news', { hello: 'world' });
+    socket.on('my other event', function (data) {
+        console.log(data);
+    });
 });
